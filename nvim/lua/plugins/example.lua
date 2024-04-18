3035388eb907c58f4df9b0cc1812898f31c8a3db
Mattia Callegari <callegari.mattia@protonmail.com>
2024-04-18 02:52:10 +0100

    update

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -120,6 +120,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "go",
         "bash",
         "html",
         "javascript",
@@ -183,6 +184,7 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "gopls",
         "stylua",
         "shellcheck",
         "shfmt",
