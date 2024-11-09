41616352eb851f2ea49919601afae141ec7861f8
Mattia Callegari <callegari.mattia@protonmail.com>
2024-11-09 14:37:28 +0100

    misc

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,26 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		require("telescope").setup({})
-		require("telescope").load_extension("fzf")
-	end,
-	keys = {
-		{ "<leader>sg", require("telescope.builtin").git_files, desc = "[s]earch telescope [g]it files" },
-		{ "<leader>sh", require("telescope.builtin").help_tags, desc = "[s]earch [h]elp tags" },
-		{ "<leader>sf", require("telescope.builtin").find_files, desc = "[s]earch [f]iles" },
-		{ "<leader>sl", require("telescope.builtin").live_grep, desc = "[s]earch [l]ive" },
-		{ "<leader>sb", require("telescope.builtin").buffers, desc = "[s]earch [b]uffers" },
-		-- { 'n', '<leader>sw', require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") }), { desc = '[s]earch [w]ord' } },
-	},
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        require("telescope").setup({})
+        require("telescope").load_extension("fzf")
+    end,
+    keys = {
+        { "<leader>sg", require("telescope.builtin").git_files, desc = "[s]earch telescope [g]it files" },
+        { "<leader>sh", require("telescope.builtin").help_tags, desc = "[s]earch [h]elp tags" },
+        {
+            "<leader>sf",
+            function()
+                require("telescope.builtin").find_files({ hidden = true })
+            end,
+            desc = "[s]earch [f]iles"
+        },
+        { "<leader>sl", require("telescope.builtin").live_grep, desc = "[s]earch [l]ive" },
+        { "<leader>sb", require("telescope.builtin").buffers,   desc = "[s]earch [b]uffers" },
+        -- { 'n', '<leader>sw', require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") }), { desc = '[s]earch [w]ord' } },
+    },
 }
